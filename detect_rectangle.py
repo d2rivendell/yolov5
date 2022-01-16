7cec7c2d2ebb8254f051fe5d90e33e61ecdd999f
d2rivendell <dream2rivendell@gmail.com>
2022-01-16 13:17:45 +0800

    裁剪出文本图片

--- detect_rectangle.py
+++ detect_rectangle.py
@@ -9,6 +9,7 @@ import sys
 from pathlib import Path
 
 import cv2
+import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
 
@@ -194,55 +195,31 @@ def run(weights=ROOT / 'best.pt',  # model.pt path(s)
         strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
     return (im0, result)
 
-
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
-    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=1, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=True, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    print_args(FILE.stem, opt)
-    return opt
-
-
 def yolo_detect(source):
     check_requirements(exclude=('tensorboard', 'thop'))
-    return run(source=source)
+    # points里面存放着检测文本坐标，分别为 (左上角，右下角)
+    rect_img, points = run(source=source)
+    origin = cv2.imread(source)
+    imgs = cut_image(origin, points)
+    return (rect_img, imgs)
 
+
+def cut_image(img, points):
+    h, w, _ = img.shape
+    imgs = []
+    for point in points:
+        left_top, right_bottom = point
+        x1, y1 = left_top
+        x2, y2 = right_bottom
+        clip = img[y1:y2, x1:x2]
+        imgs.append(clip)
+    return imgs
 
 if __name__ == "__main__":
-    # points里面存放着检测文本坐标，分别为 (左上角，右下角)
-    img, points = yolo_detect(source="/Users/leon/Desktop/yolo_val/IMG_1946.PNG")
-    # origin = cv2.imread("/Users/leon/Desktop/yolo_val/IMG_1946.PNG")
-    # for point in points:
-    #   p1, p2 = point
-    #   cv2.rectangle(origin, p1, p2, (0,0,188), thickness=1, lineType=cv2.LINE_AA)
-    # shape = origin.shape
-    # print("shape:{}".format(shape))
-    # cv2.imshow("test", origin)
-    # cv2.waitKey()  # 1 millisecond
+    rect_img, imgs = yolo_detect(source="/Users/leon/Desktop/yolo_val/IMG_1946.PNG")
+    for img in imgs:
+        cv2.imshow('test', img)
+    cv2.waitKey()
+
 
 
